6e86889c66e547a980c3be2b073b85872eadd3af
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2018-01-16 10:10:55 -0600

    completed work

---
+++ script.js
@@ -0,0 +1,5 @@
+/* git checkout branch-name
+git merge --no-ff branchname 
+-wq after to show the message and log the actual existence of the branch*/
+
+// gitkraken --> to check the log of branches.
